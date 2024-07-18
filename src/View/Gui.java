b83d062a10a1b16799642ada9fc1f533fa1dddb1
J Azul <jijoazul@gmail.com>
2024-07-18 13:03:56 -0700

    some stuff

--- src/View/Gui.java
+++ src/View/Gui.java
@@ -2,5 +2,10 @@ package View;
 //this is the gui file stuff
 
 public class Gui {
+    //images for tiles
+    //pull from maze terrains
+    //
+
+
 
 }
--- src/View/SoundManager.java
+++ src/View/SoundManager.java
@@ -1,4 +1,8 @@
 package View;
 
 public class SoundManager {
+
+    //Have mp3 files to pull from
+    //have volume slide
+
 }

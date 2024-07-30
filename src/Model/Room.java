d1911c0ef8ddb7783060009b837d3049ed6f90cb
Andrew Hwang <139718600+AndrewHwang46@users.noreply.github.com>
2024-07-29 23:01:27 -0700

    Add files via upload
    updated room

--- src/Model/Room.java
+++ src/Model/Room.java
@@ -1,30 +1,60 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class Room {
-//    private Door myDoor;
-//    private Wall myWall;
-//    private boolean myRoomMovement;
-//    private ArrayList<door>  myArrayOfDoors;
-//
-//    //Currently an empty constructor
-//    public Room (Door theDoor, Wall theWall) {
-//        myRoomMovement = true;
-//        myDoor = theDoor;
-//        myWall = theWall;
-////        myArrayOfDoors = new ArrayList<door>();
-////    }
-//
-//    public Door getDoor() {
-//        return myDoor;
-//    }
-//
-//    public void setDoor(Door theDoor){
-//        myDoor = theDoor;
-//    }
-//
-//    public boolean movementAvalible() {
-//        return myRoomMovement;
-//    }
+    private Door myDoor;
+    private boolean myRoomMovement;
+    private final Door[] myArrayOfDoors;
+    private final int myMaxRoom = 4;
+
+
+    //Currently an empty constructor
+    public Room (Door theDoor) {
+        myRoomMovement = true;
+        myDoor = theDoor;
+        myArrayOfDoors = new Door[myMaxRoom];
+        for (int i = 0; i < myMaxRoom; i++) {
+            myArrayOfDoors[i] = new Door();
+        }
+    }
+
+    public Door getDoor() {
+        return myDoor;
+    }
+
+    public void setDoor(Door theDoor){
+        myDoor = theDoor;
+    }
+
+    public boolean getRoomMovement() {
+        return myRoomMovement;
+    }
+
+    public void setRoomMovement(boolean theRoomMovement) {
+        myRoomMovement = theRoomMovement;
+    }
+
+    public void movementAvalible() {
+        for (Door myArrayOfDoor : myArrayOfDoors) {
+            if (!myArrayOfDoor.getStateOfDoor()) {
+                myRoomMovement = false;
+            }else {
+                myRoomMovement = true;
+            }
+
+        }
+
+    }
+
+    public void setMyArrayOfDoors(Door[] theArrayOfDoors) {
+        if (theArrayOfDoors.length != myMaxRoom) {
+            throw new IllegalArgumentException("Doors do not have the same length");
+        }
+        for (int i = 0; i < theArrayOfDoors.length; i++) {
+            myArrayOfDoors[i] = theArrayOfDoors[i];
+        }
+    }
 
 
 }

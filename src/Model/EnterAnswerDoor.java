7d0c7a8f6e5468d61d28c9822b103f10bda5dbc8
J Azul <jijoazul@gmail.com>
2024-07-31 16:51:48 -0700

    New door type

---
+++ src/Model/EnterAnswerDoor.java
@@ -0,0 +1,13 @@
+package Model;
+
+public class EnterAnswerDoor extends AbstractDoor{
+    @Override
+    public String questionAnswer() {
+        return null;
+    }
+
+    @Override
+    public String getDBquestion() {
+        return null;
+    }
+}

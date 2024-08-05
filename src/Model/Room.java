981282c262da3635e936fa0a561125b9feafb6f2
Andrew Hwang <139718600+AndrewHwang46@users.noreply.github.com>
2024-08-04 18:33:16 -0700

    Add files via upload

--- src/Model/Room.java
+++ src/Model/Room.java
@@ -1,8 +1,60 @@
-package model;
+package Model;
 
-public final class Room {
+import java.util.ArrayList;
 
-    public Room() {
+public class Room {
+    private Door myDoor;
+    private boolean myRoomMovement;
+    private final Door[] myArrayOfDoors;
+    private final int myMaxRoom = 4;
+
+
+    //Currently an empty constructor
+    public Room (Door theDoor) {
+        myRoomMovement = true;
+        myDoor = theDoor;
+        myArrayOfDoors = new Door[myMaxRoom];
+        for (int i = 0; i < myMaxRoom; i++) {
+            myArrayOfDoors[i] = new Door();
+        }
+    }
+
+    public Door getDoor() {
+        return myDoor;
+    }
+
+    public void setDoor(Door theDoor){
+        myDoor = theDoor;
+    }
+
+    public boolean getRoomMovement() {
+        return myRoomMovement;
+    }
+
+    public void setRoomMovement(boolean theRoomMovement) {
+        myRoomMovement = theRoomMovement;
+    }
+
+    public void movementAvalible() {
+        for (Door myArrayOfDoor : myArrayOfDoors) {
+            if (!myArrayOfDoor.getStateOfDoor()) {
+                myRoomMovement = false;
+            }else {
+                myRoomMovement = true;
+            }
+
+        }
 
     }
+
+    public void setMyArrayOfDoors(Door[] theArrayOfDoors) {
+        if (theArrayOfDoors.length != myMaxRoom) {
+            throw new IllegalArgumentException("Doors do not have the same length");
+        }
+        for (int i = 0; i < theArrayOfDoors.length; i++) {
+            myArrayOfDoors[i] = theArrayOfDoors[i];
+        }
+    }
+
+
 }

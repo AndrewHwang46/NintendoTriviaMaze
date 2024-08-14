03e543a26bd1aa176231c386f06622325eb86b89
foo-7 <derinoah004@gmail.com>
2024-08-14 10:48:23 -0700

    javadoced

--- src/Model/GameSaveAndLoad.java
+++ src/Model/GameSaveAndLoad.java
@@ -1,25 +1,41 @@
+/*
+ * T CSS 360 - Summer 2024
+ * Nintendo Trivia Maze
+ */
 package Model;
 
 import java.io.*;
 
 /**
- * GameSaveAndLoad class
+ * GameSaveAndLoad class handles with the serialization of the game.
  *
  * @author Noah Ogilvie
  */
 public final class GameSaveAndLoad {
 
+    /**
+     * A String constant that holds the serializable file name.
+     */
     private static final String FILE_NAME = "saveGame.ser";
 
+    /**
+     * A String constant that holds the initialization parameter value for the Maze class.
+     */
+    private static final String MAZE_FILE_NAME = "JIAN_MAZE_INPUT.txt";
 
-    private static final String MAZE_FILE_NAME = "ScaleDownMaze";
-
-
-
+    /**
+     * myMaze is a Maze private field.
+     */
     private Maze myMaze;
 
+    /**
+     * myPlayer is a Player private field.
+     */
     private Player myPlayer;
 
+    /**
+     * GameSaveAndLoad() is a public constructor, it initializes the private fields.
+     */
     private GameSaveAndLoad() {
         //no default constructor, no idea what the maze is originally is.
         //Putting random values to satisfy the constructor. Good luck!
@@ -27,23 +43,29 @@ public final class GameSaveAndLoad {
         this.myPlayer = new Player();
     }
 
-    public final void saveGame() {
+    /**
+     * saveGame() method serializes the game into a file.
+     */
+    public void saveGame() {
         try (FileOutputStream savingFile = new FileOutputStream(FILE_NAME);
-             ObjectOutputStream out = new ObjectOutputStream(savingFile); ) {
+             ObjectOutputStream out = new ObjectOutputStream(savingFile)) {
             out.writeObject(myMaze);
             out.writeObject(myPlayer);
         } catch (final IOException e) {
-            System.out.println("Error saving game");
+            System.out.println("Error saving game" + e.getMessage());
         }
     }
 
-    public  final void loadGame() {
+    /**
+     * loadGame() method deserializes the file and the game data is returned.
+     */
+    public void loadGame() {
         try (FileInputStream loadingFile = new FileInputStream(FILE_NAME);
-             ObjectInputStream in = new ObjectInputStream(loadingFile); ) {
+             ObjectInputStream in = new ObjectInputStream(loadingFile)) {
             myMaze = (Maze) in.readObject();
             myPlayer = (Player) in.readObject();
         } catch (final IOException | ClassNotFoundException e) {
-            System.out.println("Error loading game");
+            System.out.println("Error loading game" + e.getMessage());
         }
     }
 }

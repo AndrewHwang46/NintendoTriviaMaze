8aa91713808d9dfc17b92863064c9b04d75e7a6e
foo-7 <derinoah004@gmail.com>
2024-08-15 15:26:06 -0700

    javadoc conflicted changed, agreed with Andrew

--- src/Model/GameSaveAndLoad.java
+++ src/Model/GameSaveAndLoad.java
@@ -7,68 +7,110 @@ package Model;
 import java.io.*;
 
 /**
- * GameSaveAndLoad class uses serialization and
- * deserialization in order to save and load the game.
+ * GameSaveAndLoad class uses serialization and deserialization
+ * for the save and load.
  *
  * @author Noah Ogilvie
  * @author Andrew Hwang
- * @version 2.0
+ * @version 3.0
  */
 public final class GameSaveAndLoad {
 
+    /**
+     * String constant that is the game's load and save
+     * file name.
+     */
     private static final String FILE_NAME = "saveGame.ser";
+
+    /**
+     * String constant that is a file for the Maze constructor.
+     */
     private static final String MAZE_FILE_NAME = "ScaleDownMaze.txt";
 
+    /**
+     * mySaved is a boolean private field.
+     */
     private Boolean mySaved;
+
+    /**
+     * myMaze is a Maze private field.
+     */
     private Maze myMaze;
+
+    /**
+     * myPlayer is a Player private field.
+     */
     private Player myPlayer;
 
+    /**
+     * GameSaveAndLoad() constructor initializes the global private fields.
+     */
     public GameSaveAndLoad() {
-        mySaved = false;
+        //mySaved = false;
         this.myMaze = new Maze(MAZE_FILE_NAME);
         this.myPlayer = Player.getInstance();
     }
 
+    /**
+     * saveGame() uses serialization to save the Maze and Player.
+     * @return whether if the game has happened to be saved or not.
+     */
     public boolean saveGame() {
-        boolean saved = true;
+        boolean gameSaved = false;
         try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(FILE_NAME))) {
             out.writeObject(myMaze);
             out.writeObject(myPlayer);
-            mySaved = saved;
+            gameSaved = true;
+            mySaved = true;
         } catch (IOException e) {
             System.err.println("Error saving game: " + e.getMessage());
-            saved =  false;
         }
-        return saved;
+        return gameSaved;
     }
 
+    /**
+     * loadGame() uses deserialization to load the MAze and Player.
+     * @return whether if the game has happened to load or not.
+     */
     public boolean loadGame() {
-        boolean load = true;
+        boolean gameLoaded = false;
         try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(FILE_NAME))) {
             Maze loadedMaze = (Maze) in.readObject();
             Player loadedPlayer = (Player) in.readObject();
 
-            // Update the singleton instances
             Maze.setMazeSingleton(loadedMaze);
             Player.getInstance().updateState(loadedPlayer);
 
-            // Update local references
             this.myMaze = loadedMaze;
             this.myPlayer = Player.getInstance();
+
+            gameLoaded = true;
         } catch (IOException | ClassNotFoundException e) {
             System.err.println("Error loading game: " + e.getMessage());
-            load = false;
         }
-        return load;
+        return gameLoaded;
     }
 
+    /**
+     * getMaze() gets the Maze object.
+     * @return the Maze object.
+     */
     public Maze getMaze() {
         return myMaze;
     }
 
+    /**
+     * getPlayer() gets the Player object.
+     * @return the Player object.
+     */
     public Player getPlayer() {
         return myPlayer;
     }
+
+    /**
+     * getSaved() gets whether if the game got saved or not.
+     * @return the boolean value of mySaved.
+     */
     public Boolean getSaved() {
         return mySaved;
     }

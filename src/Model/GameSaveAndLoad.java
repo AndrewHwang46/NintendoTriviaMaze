8f076df13ba1f1e080710539e90b18791e21816c
foo-7 <derinoah004@gmail.com>
2024-08-15 15:22:24 -0700

    finished junit testing, javadoc, and fixed return statements in GameSaveAndLoad class

--- src/Model/GameSaveAndLoad.java
+++ src/Model/GameSaveAndLoad.java
@@ -16,19 +16,45 @@ import java.io.*;
  */
 public final class GameSaveAndLoad {
 
+    /**
+     * String constant that is the game's load and save
+     * file name.
+     */
     private static final String FILE_NAME = "saveGame.ser";
+
+    /**
+     * String constant that is a file for the Maze constructor.
+     */
     private static final String MAZE_FILE_NAME = "ScaleDownMaze.txt";
 
+    /**
+     * mySaved is a boolean private field.
+     */
     private Boolean mySaved;
+
+    /**
+     * myMaze is a Maze private field.
+     */
     private Maze myMaze;
+
+    /**
+     * myPlayer is a Player private field.
+     */
     private Player myPlayer;
 
+    /**
+     * GameSaveAndLoad() constructor initializes the global private fields.
+     */
     public GameSaveAndLoad() {
         mySaved = false;
         this.myMaze = new Maze(MAZE_FILE_NAME);
         this.myPlayer = Player.getInstance();
     }
 
+    /**
+     * saveGame() uses serialization to save the Maze and Player.
+     * @return whether if the game has happened to be saved or not.
+     */
     public boolean saveGame() {
         boolean saved = true;
         try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(FILE_NAME))) {
@@ -42,6 +68,10 @@ public final class GameSaveAndLoad {
         return saved;
     }
 
+    /**
+     * loadGame() uses deserialization to load the MAze and Player.
+     * @return whether if the game has happened to load or not.
+     */
     public boolean loadGame() {
         boolean load = true;
         try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(FILE_NAME))) {
@@ -62,13 +92,26 @@ public final class GameSaveAndLoad {
         return load;
     }
 
+    /**
+     * getMaze() gets the Maze object.
+     * @return the Maze object.
+     */
     public Maze getMaze() {
         return myMaze;
     }
 
+    /**
+     * getPlayer() gets the Player object.
+     * @return the Player object.
+     */
     public Player getPlayer() {
         return myPlayer;
     }
+
+    /**
+     * getSaved() gets whether if the game got saved or not.
+     * @return the boolean value of mySaved.
+     */
     public Boolean getSaved() {
         return mySaved;
     }
